b3fa4b4c0fe2c2df30d650e3eaae463ef7c0ae88
Amelia <brown.amelia@gmail.com>
2019-08-21 10:55:26 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='brutils',
+    version='0.1dev',
+    packages=['brutils',],
+    license='WTFPL',
+    # long_description=open('README.txt').read(),
+)
